16ba20a5148220ed9b185ef7b11cbd51378f2053
Alex Aloia <tripl3infinity@gmail.com>
2014-10-12 04:35:26 -0500

    deploy task works!

--- roles/wordpress/templates/template_wp-config.php
+++ roles/wordpress/templates/template_wp-config.php
@@ -1,8 +1,5 @@
 <?php
 
-{% if env != 'prod' %}
-{% set domain = env + '.' %}
-{% endif %}
 
 /* -------------------------->
 // DEBUG OPTIONS
@@ -12,13 +9,25 @@ define( 'WP_DEBUG_DISPLAY', false );
 define('SAVEQUERIES', true);
 < -------------------------- */
 
-//define('WP_ENV', 'development');
-define('WP_HOME','http://{{ domain | default('') }}{{ wp_home_url }}');
-define('WP_SITEURL','http://{{ domain | default('') }}{{ wp_home_url }}/wordpress');
+/* ---------------------------------------------------------------------------------------------------------------->
+  Depending on your server configuration, you may find WordPress fails to find your content (themes and plugins).
+  This is due to how your server returns `$_SERVER['DOCUMENT_ROOT']`. If this issue affects you, try swapping
+  for the `dirname(__FILE__)` method below.
+ <---------------------------------------------------------------------------------------------------------------- */
 
-define('DB_NAME', '{{env}}_{{wp_project_name}}');
+{% if project.env != 'prod' %}
+{% set domain = project.env + '.' %}
+define('WP_HOME','http://{{ domain | default('') }}{{ project.home_url }}');
+define('WP_SITEURL','http://{{ domain | default('') }}{{project.home_url}}/wordpress');
+define('WP_CONTENT_URL', 'http://{{ domain | default('') }}{{ project.home_url }}/content');
+{% endif %}
+//define('WP_CONTENT_DIR', realpath($_SERVER['DOCUMENT_ROOT'] . '/content'));
+define('WP_ENV', '{{project.env}}');
+define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/content' );
+
+define('DB_NAME', '{{project.env}}_{{project.name}}');
 define('DB_USER', '{{ deploy_user }}');
-define('DB_PASSWORD', '{{ wp_db_pass }}');
+define('DB_PASSWORD', '{{ project.db_pass }}');
 
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
@@ -29,23 +38,17 @@ $table_prefix  = 'wp_';
 // SALTS GO HERE
 {{ wp_salt.stdout }}
 
-/* ---------------------------------------------------------------------------------------------------------------->
-  Depending on your server configuration, you may find WordPress fails to find your content (themes and plugins).
-  This is due to how your server returns `$_SERVER['DOCUMENT_ROOT']`. If this issue affects you, try swapping
-  for the `dirname(__FILE__)` method below.
- <---------------------------------------------------------------------------------------------------------------- */
 
-define('WP_CONTENT_URL', 'http://{{ domain | default('') }}{{ wp_home_url }}/content');
-define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/content' );
-//define('WP_CONTENT_DIR', realpath($_SERVER['DOCUMENT_ROOT'] . '/content'));
 
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 require_once(ABSPATH . 'wp-settings.php');
 
-
-/** Disable Automatic Updates Completely */
-define( 'AUTOMATIC_UPDATER_DISABLED', {{auto_up_disable}} );
+{% if project.auto_update == true %}
+define( 'AUTOMATIC_UPDATER_DISABLED', false );
+{% else %}
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
+{% endif %}
 
 
 
