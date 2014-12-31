86dc4d00dd1effb3ebd7f35c8391647bc0189549
Alex Aloia <tripl3infinity@gmail.com>
2014-12-31 17:16:21 -0600

    finished with verification rework -- need to clean up before merging back to master

--- roles/wordpress/templates/template_wp-config.php
+++ roles/wordpress/templates/template_wp-config.php
@@ -24,6 +24,17 @@ define('WP_SITEURL','http://{{ domain | default('') }}{{project.home_url}}/wordp
 define('WP_CONTENT_URL', 'http://{{ domain | default('') }}{{ project.home_url }}/content');
 {% endif %}
 
+
+
+{% if project.env == 'prod' %}
+define('WP_HOME','http://{{ project.home_url }}');
+define('WP_SITEURL','http://{{project.home_url}}/wordpress');
+define('WP_CONTENT_URL', 'http://{{ project.home_url }}/content');
+{% endif %}
+
+
+
+
 // this one was commented out
 //define('WP_CONTENT_DIR', realpath($_SERVER['DOCUMENT_ROOT'] . '/content'));
 define('WP_ENV', '{{project.env}}');
